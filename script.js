815f92c91616690b85ef3c529aa66d99c82cc4b6
Glamwithlia1 <abatluv@gmail.com>
2025-10-27 15:43:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
-  //form submission
+//form submission
 document.getElementById("lead-form")
 .addEventListener("submit",function(e) {
     e.preventDefault();
-// get form values  
+
+    // get form values  
 const firstname = this.elements[0].value;
 const lastname = this.elements[1].value;
 const email = this.elements[2].value;
@@ -21,23 +21,8 @@ this.reset()
 }
 )
 
-// show popup after 5 seconds 
-setTimeout(function (){
-document.getElementById("email-popup").style.display="flex"
-},5000)
 
-//close popup when X is clicked
-document.querySelector(".close-btn").addEventListener("click",function(){
-    document.getElementById("email-popup").style.display="none";
 
-})
-
-// close when clicking outside content  
-document.getElementById("email-popup").addEventListener("click",function(e){
-    if(e.target===this){
-        this.style.display="none";
-    }
-});
 
 
 // ==============================> Cookies <======================================
@@ -52,18 +37,20 @@ document.getElementById("email-popup").addEventListener("click",function(e){
 function declineCookies() {
     document.getElementById("cookie-banner").style.display="none";
     localStorage.setItem("cookiesAccepted", "false")
-
+// loadAnalytics(); //GA4
     }
    
     // on page load,check past choice
     window.onload=function() {
 
-        if(this.localStorage.getItem("cookiesAccepted")==="true"){
+        if(localStorage.getItem("cookiesAccepted")==="true"){
             loadAnalytics();
-          document.getElementById("cookie-banner").style.display ="none"; 
-          
+            document.getElementById("cookie-banner").style.display ="none";
+
            }else if(localStorage.getItem("cookiesAccepted")=="false"){
             document.getElementById("cookie-banner").style.display ="none";
            }
         }
 
+        // footer Year //
+       document.getElementById("year").textContent = new Date().getFullYear();
